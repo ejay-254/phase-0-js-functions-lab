766b7f0dcbc2bcde77d15f3bf1e4e34aa6d2836d
ejay-254 <ellacherop@gmail.com>
2026-01-11 15:30:11 +0100

    use the correct functions

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+function calculateTax(amount) {
+  return amount * 0.1;
+}
 
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
 
+function findMaximum(num1, num2) {
+  if (num1 > num2) return num1;
+  return num2;
+}
 
+function isPalindrome(word) {
+  return word === word.split("").reverse().join("");
+}
 
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  return originalPrice - (originalPrice * discountPercentage / 100);
+}
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
